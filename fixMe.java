10c128d0b4707aa629664e1de1d5cfbb0a4f58cc
MehrabToghani <Mehrab.toghi@gmail.com>
2018-12-26 13:01:29 +0330

    done

--- fixMe.java
+++ fixMe.java
@@ -3,9 +3,10 @@ package myapplication;
 import java.awt.*;
 public class MyApplication {
 static final String startStr="\n****start of the program****";
-final String endStr="\n****end of the program****";
-public void main(String []arg){
-		printMessage(startStr);
+final static String endStr="\n****end of the program****";
+public static void main(String []arg){
+    MyApplication SB=new MyApplication();
+		SB.printMessage(startStr);
 		Square s=new Square(100);
 		System.out.println(s.computePerimeter());
 		System.out.println(s.computeSurface());
@@ -13,7 +14,6 @@ public void main(String []arg){
 		Point p2=new Point(10.1,10.2);
 		ColorPoint cp1=new ColorPoint(1,1);
 		ColorPoint cp2=new ColorPoint(1,1,new Color(12,34,56));
-		cp1=p1;
 		p1=cp1;
 	MyApplication myA=new MyApplication();
 		myA.printMessage(endStr);
@@ -24,15 +24,15 @@ private void printMessage(String str)
 	System.out.println(str);
 }
 ////////////////////////////////
-class Square {
+static class Square {
 	public Square(int init_x){x=init_x;}
-	public int computeSurface(){return x**x;}
+	public int computeSurface(){return x*x;}
 	public int computePerimeter(){return 4*x;}
 	private int x;
 }//square
 }//myApplication
 class Point{
-	public Point(int init_x,int init_y){
+	public Point(double init_x,double init_y){
 		x=init_x;
 		y=init_y;
 	}
@@ -40,12 +40,16 @@ class Point{
 		x+=init_x;
 		y+=init_y;
 	}
-	protected int x,y;
+	protected double x,y;
 }//point
 class ColorPoint extends Point{
 	public ColorPoint(int init_x,int init_y){
 		super(init_x,init_y);
-		color=Color.Blue;
+		color=Color.blue;
+	}
+        public ColorPoint(int init_x,int init_y , Color color){
+		super(init_x,init_y);
+		this.color = color ;
 	}
 	Color color;
 }//ColorPoint
